9f6acb38116473e034ea424ad46f41f31d7903bf
Hemant <sahuhemant770@gmail.com>
2023-05-28 16:37:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import List from './List';
-import Edit from './Edit';
-import Rbs from './Rbs';
-import { useState } from 'react';
+import Main from './Main';
 
 function App() {
-  const [Name,setName]=useState('');
-  const [showList,setShowList]=useState(true);
-  const [showEdit,setShowEdit]=useState(false);
-  const [showRbs,setShowRbs]=useState(false);
-  const [newName,setnewName]=useState('');
-  const reset=()=>{
-    setShowEdit(false);
-    setShowList(false);
-    setShowRbs(false);
-  }
-  const showPage=(page)=>{
-      reset();
-      if(page=="edit"){
-      setShowEdit(true);
-      }
-
-      if(page=="rbs"){
-        setShowRbs(true);
-      }
-
-      if(page=="list"){
-        setShowList(true);
-      }
-  }
-  
+ 
   return (
-    <>
-      {showList && <List setnewName={setnewName} Name={Name} showPage={showPage}  changeName={setName} ></List> }
-      {showEdit && <Edit showPage={showPage} setnewName={setnewName} ConstName={Name} Name={newName}></Edit>}
-      {showRbs &&  <Rbs Name={newName} showPage={showPage}></Rbs>}
-    </>
+     <Main></Main>
   );
 }
 
